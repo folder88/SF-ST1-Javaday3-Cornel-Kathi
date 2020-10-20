4a721119038954282655cdefd15faff7405a07a4
Scheyk <kathrin.dostal@gmail.com>
2020-10-20 11:08:38 +0200

    finish basic task one and two with commits

--- src/Kathi/BasicOne.java
+++ src/Kathi/BasicOne.java
@@ -3,33 +3,39 @@ package Kathi;
 import java.util.Scanner;
 
 /**
- * this is a program that saves a number in a variable and prints whether it is
+ * this is a program that saves a number in a variable and prints whether it is   <--- the doc comment
  * positive or negative *
  */
 
 public class BasicOne {
         /*
-            Example: for input number 25, expected output should be:
+            Example: for input number 25, expected output should be:    <---- the multicomment to descripe the task
             Number is positive
         */
 
-    static void info(int b){
-        if(b == 25){
+   /* static void info(int b){
+        if(b == 25){                        <-----a mulitiline comment to comment out
             System.out.println("positiv");
         }
-    }
+    }*/
 
     public static void main(String[]args) {
-        //int some = info();
+        //int some = info();  <---singe comment to comment out
 
         Scanner in = new Scanner(System.in);
-        System.out.println("enter the number 25");
+        System.out.println("enter a number");
         int a = in.nextInt();
 
-        //System.out.println("Number is positiv");
+        if(a == 25){
+            System.out.println("Number is positiv");
+        } else {
+            System.out.println("Number is negativ");
+        }
+
+
 
        /* if(a == 25){
-            System.out.println("Number is positiv");
+            System.out.println("Number is positiv");  <-----a mulitiline comment to comment out
         }*/
 
 
--- src/Kathi/BasicTwo.java
+++ src/Kathi/BasicTwo.java
@@ -1,8 +1,34 @@
 package Kathi;
 
+import java.util.Scanner;
+
 public class BasicTwo {
+
 
     public static void main(String[]args){
 
+        /**
+         * Write a Java program that reads a floating-point number and prints “zero”
+         * if the number is zero. Otherwise, print “positive” or “negative”.                              <--- the doc comment
+         * Add “small” if the value of the number is less than 1, or “large” if it exceeds 1,000,000.
+         */
+
+        Scanner in = new Scanner(System.in);         // <---- var Scanner to get the input
+        System.out.println("input some number");
+        float some = in.nextFloat();                // <--- get the ral input
+
+
+        //################################## the if function to compare the input
+        if(some == 0){
+            System.out.println("zero");
+        } else if(some < 0) {
+            System.out.println(some + " is a negative number");
+        } else if(some < 1) {
+            System.out.println(some +" is a small number");
+        } else if(some > 1000000) {
+            System.out.println(some + " is a large positiv number");
+        } else {
+            System.out.println("positiv");
+        }
     }
 }
